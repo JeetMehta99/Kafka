88e8ecd9cf84b501c7c965182a7d4a43970c0bfd
Jeet <jmrockstat06@gmail.com>
2021-12-03 15:51:40 -0600

    Utilities required for kafka

---
+++ kafka-installation/Kafka/KafkaUtility.scala
@@ -0,0 +1,29 @@
+
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+
+
+object KafkaUtility {
+
+  private val kafkaParams = Map(
+    "bootstrap.servers" -> "localhost:9092",
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "auto.offset.reset" -> "earliest",
+    "group.id" -> "log-gen"
+  )
+
+  private val preferredHosts = LocationStrategies.PreferConsistent
+
+
+  def createDStreamFromKafka(ssc: StreamingContext, topics: List[String]): InputDStream[ConsumerRecord[String, String]] =
+    KafkaUtils.createDirectStream[String, String](
+      ssc,
+      preferredHosts,
+      ConsumerStrategies.Subscribe[String, String](topics.distinct, kafkaParams)
+    )
+
+}
